e55c709dba0770198513c66ee10e32fc97a39c59
Michał Zarzycki <s184856@o365.student.pg.edu.pl>
2021-03-03 19:55:22 +0100

    Dodaj pliki projektów.

---
+++ robot-droga/stdafx.h
@@ -0,0 +1,23 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+#include <iostream>
+#include <stdio.h>
+#include <tchar.h>
+#include <cstdlib>
+#include <time.h>
+#include <cmath>
+#include <windows.h>
+#include <iomanip>
+#include <iostream>
+#include <random>
+#include <string>
+
+
+
+// TODO: reference additional headers your program requires here
