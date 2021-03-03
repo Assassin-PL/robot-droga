b6fbc64cb33db699aa979ff47ea444dfe165faaf
Pocwiardo <80002864+Pocwiardo@users.noreply.github.com>
2021-03-03 20:55:39 +0100

    Update robot-droga.cpp

--- robot-droga/robot-droga.cpp
+++ robot-droga/robot-droga.cpp
@@ -5,12 +5,20 @@
 
 using namespace std;
 
+#define segSize 5
+#define segIl 6
+
 int main()
 {
     cout << "Hello World!\n";
     cout << "Witaj janusz!\n";
     cout << "Witaj!1213\n";
     cout << "Pisze cos"<<endl;
+    bool rzad1[segIl][segSize] = { {1,1,0,1,1},{1,1,0,0,1},{0,0,0,0,0},{1,1,0,0,0},{0,0,0,1,1},{1,1,0,1,1} };
+    bool rzad2[segIl][segSize] = { {1,0,0,1,1},{0,1,1,0,1},{0,0,1,0,0},{1,0,0,1,0},{1,0,0,0,1},{1,1,0,1,1} }; // na czarno 1, na biało 0, jak dla mnie bardziej intuicyjne
+    bool rzad3[segIl][segSize] = { {0,0,1,0,0},{0,0,0,0,0},{0,0,1,1,0},{0,0,1,0,0},{0,0,1,0,0},{0,0,0,0,0} };
+    bool rzad4[segIl][segSize] = { {1,0,0,0,1},{1,0,0,0,1},{1,0,1,1,1},{0,1,0,0,1},{0,0,1,0,0},{1,1,0,1,1} };
+    bool rzad5[segIl][segSize] = { {1,1,0,1,1},{1,1,0,0,1},{1,0,0,0,0},{1,0,0,1,1},{0,0,0,0,1},{1,1,0,1,1} };
     return 0;
 }
 
